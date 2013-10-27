421171bb4c96bf21cfd8d70437e0871eed499220
Dev Bootcamp <Apprentice@dbc33.local>
2013-10-27 13:45:27 -0700

    changed the redirect in the comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,18 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with_name: "laura", password: "elephants", only: :destroy
+  http_basic_authenticate_with name: "laura", password: "elephants", only: :destroy
 
   def create
     @post = Post.find(params[:post_id])
     @comment =
     @post.comments.create(params[:comment].permit(:commenter, :body))
-    redirect_to_post_path(@post)
+    redirect_to post_path(@post)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
-    redirect_to_post_path(@post)
+    redirect_to post_path(@post)
   end
 end
 
